60b86a8edaad7fe4e16db5ac4669d9db7611a50e
matiasnnr <matias.nnr@hotmail.com>
2021-02-23 21:31:51 -0300

    add: react dropzone for upload photos from pc

--- src/components/User/AvatarForm/AvatarForm.js
+++ src/components/User/AvatarForm/AvatarForm.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './AvatarForm.scss';
 import { Button } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
 
 const AvatarForm = (props) => {
 
     const { setShowModal } = props;
+    const onDrop = useCallback((acceptedFile) => {
+        console.log(acceptedFile);
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/jpeg, image/jpg, image/png',
+        noKeyboard: true,
+        multiple: false, // para que solo se pueda subir uno
+        onDrop
+    })
 
     return (
         <div className="avatar-form" >
-            <Button>Cargar una foto</Button>
+            <Button {...getRootProps()} >Cargar una foto</Button>
             <Button>Eliminar foto actual</Button>
             <Button onClick={() => setShowModal(false)} >Cancelar</Button>
+            <input {...getInputProps()} />
         </div>
     )
 }

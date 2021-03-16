7127bf630d5576f1ae37dc1c3ce4320560a91205
matiasnnr <matias.nnr@hotmail.com>
2021-03-16 12:18:25 -0300

    add: publications feed on home page

---
+++ src/components/Home/Feed/Feed.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { map } from 'lodash';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { Image } from 'semantic-ui-react';
+import { GET_PUBLICATIONS_FOLLOWEDS } from '../../../gql/Publication';
+import Action from '../../Modal/ModalPublication/Action';
+import CommentForm from '../../Modal/ModalPublication/CommentForm';
+import ModalPublication from '../../Modal/ModalPublication';
+import NotFound from '../../../assets/images/avatar.png';
+import './Feed.scss';
+
+const Feed = () => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [publicationSelected, setPublicationSelected] = useState(null);
+    const { data, loading, startPolling, stopPolling } = useQuery(GET_PUBLICATIONS_FOLLOWEDS);
+
+    useEffect(() => {
+        startPolling(1000);
+        return () => {
+            stopPolling();
+        }
+    }, [startPolling, stopPolling])
+
+    if (loading) return null;
+
+    const { getPublicationsFolloweds } = data;
+
+    const openPublication = (publication) => {
+        setPublicationSelected(publication);
+        setShowModal(true);
+    }
+
+    return (
+        <>
+            <div className="feed" >
+                {
+                    map(getPublicationsFolloweds, (publication, index) => (
+                        <div key={index} className="feed__box" >
+                            <Link to={`/${publication.idUser.username}`} >
+                                <div className="feed__box-user">
+                                    <Image
+                                        src={publication.idUser.avatar || NotFound}
+                                        avatar
+                                    />
+                                    <span>{publication.idUser.name}</span>
+                                </div>
+                            </Link>
+
+                            <div
+                                className="feed__box-photo"
+                                style={{ backgroundImage: `url("${publication.file}")` }}
+                                onClick={() => openPublication(publication)}
+                            />
+
+                            <div className="feed__box-actions">
+                                <Action publication={publication} />
+                            </div>
+
+                            <div className="feed__box-form" >
+                                <CommentForm publication={publication} />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            {
+                showModal
+                &&
+                <ModalPublication
+                    show={showModal}
+                    setShow={setShowModal}
+                    publication={publicationSelected}
+                />
+            }
+        </>
+    )
+}
+
+export default Feed;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import Feed from '../../components/Home/Feed';
+import { useQuery } from '@apollo/client';
 import './Home.scss';
 
 const Home = () => {
 
     return (
-        <div>
-            <h1>Estamos en Home</h1>
-        </div>
+        <Grid className="home" >
+            <Grid.Column className="home__left" width={11} >
+                <Feed />
+            </Grid.Column>
+            <Grid.Column className="home__right" width={5} >
+                <h2>Usuarios no seguidos</h2>
+            </Grid.Column>
+        </Grid>
     )
 }
 
